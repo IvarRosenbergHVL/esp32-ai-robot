0ebd713ea96726de61474129b2a5e60d2a29d8b3
Ivar Rosenberg <39551312+IvarRosenbergHVL@users.noreply.github.com>
2026-09-12 17:58:02 +0200

    feat: extend firmware/dualeye_mvp/dualeye_mvp.ino

--- firmware/dualeye_mvp/dualeye_mvp.ino
+++ firmware/dualeye_mvp/dualeye_mvp.ino
@@ -1,14 +1,21 @@
 #include "LCD_Driver.h"
 #include "LVGL_Driver.h"
 #include "LVGL_Example.h"
+#include "Button_Driver.h"
+#include "Board_Diagnostics.h"
+#include "SD_Card.h"
 
 void setup()
 {
   Serial.begin(115200);
   delay(250);
   Serial.println("[robot] Starting DualEye MVP");
+  Board_Diagnostics_Run();
+  SD_Init();
+  I2C_Init();
   LCD_INIT();
   Lvgl_Init();
+  Button_Init();
   Lvgl_Example1();
 
   vTaskDelay(pdMS_TO_TICKS(100));
@@ -17,5 +24,10 @@ void setup()
 }
 
 void loop() {
+  if (BOOT_KEY_State == Click) {
+    BOOT_KEY_State = None;
+    Eye_Notice();
+    Serial.println("[robot] BOOT click: notice animation");
+  }
   vTaskDelay(pdMS_TO_TICKS(5));
 }
